9bb2805546dcd590aac7f997e3323df20a8ddb03
Reece Balfour <reece@RBMP.local>
2025-10-29 15:00:05 +0000

    prevent crowding for collectionsgrid on small screens

--- src/app/collections/CollectionsGrid.tsx
+++ src/app/collections/CollectionsGrid.tsx
@@ -39,7 +39,7 @@ export default function CollectionsGrid({
   }
 
   return (
-    <div className="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {optimisticCollections.map((c) => (
         <CollectionCard
           key={c.id}
