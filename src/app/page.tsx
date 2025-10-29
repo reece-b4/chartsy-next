5e8cc6e9dd168f0d038fd7932e3887f053eba64a
Reece Balfour <reece@RBMP.local>
2025-10-29 16:36:53 +0000

    style root page.module to make consistent with child pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,8 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        {/* do links directly replace collections here ie, app layout persists, div and main tags above persist? */}
         <Collections />
       </main>
-      {/* <footer className={styles.footer}>
-      </footer> */}
     </div>
   );
 }
